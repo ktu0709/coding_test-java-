dc207624b686357aa35a55762caa2c54f422344e
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2023-12-11 23:00:51 +0900

    Create answer.java

---
+++ 11659/answer.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Main {
+	public static void main(String[] args) throws java.lang.Exception {
+     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	 StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+	 
+	 int suno = Integer.parseInt(stringTokenizer.nextToken());
+	 int quizno = Integer.parseInt(stringTokenizer.nextToken());
+	 long[] nnum = new long[suno+1];
+	 stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+	 
+	 // i번째까지 숫자배열의 합
+	 for(int i = 1;i<=suno;i++){
+	 	nnum[i] = nnum[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+	 	//System.out.println(i+": "+nnum[i] +" : " +nnum[i-1]);
+	 }
+	 
+	 for(int q=0;q<quizno;q++){
+	 	stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+	 	int i = Integer.parseInt(stringTokenizer.nextToken());
+	 	int j = Integer.parseInt(stringTokenizer.nextToken());
+	 	System.out.println(nnum[j] - nnum[i-1]);
+	 }
+    
+    }
+}
