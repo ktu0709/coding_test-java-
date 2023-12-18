e0e21bfaa66b453554127512593fff16ef5d6502
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2023-12-18 11:03:40 +0900

    Create 1.java

---
+++ 구름코딩테스트_러닝클래스/1.java
@@ -0,0 +1,44 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		//String input = br.readLine();
+		//System.out.println("Hello Goorm! Your input is " + input);
+		int t1 = Integer.parseInt(st.nextToken());
+		int t2 = Integer.parseInt(st.nextToken());
+		
+		int[] box = new int[t1];
+		
+		for(int i=0;i<t2;i++){
+			st = new StringTokenizer(br.readLine());
+			int check1 = Integer.parseInt(st.nextToken()); // 상자번호
+			int check2 = Integer.parseInt(st.nextToken()); // 명령어 순번
+			int check3 = Integer.parseInt(st.nextToken()); // 공 개수
+			
+			//System.out.println("\n"+"입력값 : " + check1 + " : " +check2+" : "+check3);
+			
+			if(check2 == 1){
+				box[check1-1] += check3;
+			}
+			else if(check2 == 2){
+				if(box[check1-1] < check3){
+		
+				}
+				else {box[check1-1] -= check3;}				
+			}			
+		}
+		
+		for(int i=0;i<t1;i++){
+			System.out.println(box[i]);
+		}
+	}
+}
