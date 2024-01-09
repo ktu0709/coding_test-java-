3db8b65d362c2b0d8a14704eabc22a2c13f712e4
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2024-01-09 21:51:08 +0900

    Create answer.java

---
+++ CodingTest_Book/12891/answer.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	 StringTokenizer st = new StringTokenizer(bf.readLine());
+	 
+	 int t1 = Integer.parseInt(st.nextToken());
+	 int t2 = Integer.parseInt(st.nextToken());
+	 int count =0;
+	 
+	 st = new StringTokenizer(bf.readLine());
+	 
+	 String password = st.nextToken();
+	 char[] arr = password.toCharArray();
+	 
+	 st = new StringTokenizer(bf.readLine());
+	 int a_num = Integer.parseInt(st.nextToken());
+	 int c_num = Integer.parseInt(st.nextToken());
+	 int g_num = Integer.parseInt(st.nextToken());
+	 int t_num = Integer.parseInt(st.nextToken());
+	
+	 
+	 
+	}
+}
