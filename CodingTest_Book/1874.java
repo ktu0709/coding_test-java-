8c3a122e11beb3f9447bba2457ea72e2546c84a8
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2024-01-17 16:06:28 +0900

    recode

--- CodingTest_Book/1874.java
+++ CodingTest_Book/1874.java
@@ -5,47 +5,54 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Main
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	 BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-	 StringTokenizer st = new StringTokenizer(bf.readLine());
-	 StringBuffer buf = new StringBuffer();
-	 Stack<Integer> stack = new Stack<>();
-	 int num = 1;
-	 boolean res = true;
-	 int t1 = Integer.parseInt(st.nextToken());
-	 int[] arr = new int[t1];
-	 
-	 for(int i=0;i<t1;i++){
-	 	st = new StringTokenizer(bf.readLine());
-	 	arr[i] =Integer.parseInt(st.nextToken());
-	 }
-	 
-	 for(int i=0;i<arr.length;i++){
-	 	int su = arr[i];
-	 	if(su >= num){
-	 		while(su >= num){
-	 			stack.push(num++);
-	 			buf.append("+\n");
-	 		}
-	 		stack.pop();
-	 		buf.append("-\n");
-	 	}
-	 	else{
-	 		int n = stack.pop();
-	 		
-	 		if(n> su){
-	 			System.out.println("NO");
-	 			res = false;
-	 			break;
-	 		}
-	 		else{
-	 			buf.append("-\n");
-	 		}
-	 	}
-	 }
-		if(res) System.out.println(buf.toString());
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuffer sb = new StringBuffer();
+    Stack<Integer> stack = new Stack<>();
+    
+    int num = 1;
+    int t1 = Integer.parseInt(st.nextToken());
+    int[] arr = new int[t1];
+    boolean res = true;
+    for(int i=0;i<t1;i++){
+    	st= new StringTokenizer(br.readLine());
+    	arr[i] = Integer.parseInt(st.nextToken());
+    }
+    
+    for(int i=0;i<t1;i++){
+    	
+    	if(num <= arr[i]){
+    		
+    	while(num <= arr[i]){
+    		stack.push(num++);
+    		sb.append("+\n");
+    	}
+    	 stack.pop();
+    	 sb.append("-\n");
+    	}
+    	else{
+    		int n = stack.pop();
+
+    		if(n> arr[i]){
+    			System.out.println("NO");
+    			res = false;
+    			break;
+    		}
+    		
+    		else{
+    			sb.append("-\n");
+    		}
+    		
+    		
+    	
+    	}
+    
+     if(res) System.out.println(sb.toString());
+    }
+    
 	}
 }
