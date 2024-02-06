91a45bc02193d644a309d2b6d273d593be7ee21e
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2024-02-06 21:09:26 +0900

    update 17298

--- CodingTest_Book/17298_ing.java
+++ CodingTest_Book/17298_ing.java
@@ -23,36 +23,30 @@ class Ideone
 			t2[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		while(cnt <= t1){
+		while(cnt < t1){
 			check_num = t2[cnt];
-			
+			//System.out.println("cnt : "+cnt +"," +check_num);
 			if(que.isEmpty()){  //큐가 비어있음 
 				que.add(check_num);
 			}
 			
 			for(int j=cnt+1;j<=t1;j++){
-				System.out.println("j: "+j);
+				//System.out.println("j: "+j);
 				if(j==t1){
-					System.out.println(-1);
+					System.out.print(-1+" ");
 					break;
 				}
-				if(que.peek() < check_num){
-					System.out.println(check_num);
-					que.add(check_num);
+				if(que.peek() < t2[j]){
+					System.out.print(t2[j]+" ");
+					que.add(t2[j]);
 					que.remove();
 					break;
 				}
-				//System.out.println("j: "+j);
-				//System.out.println("t2[j]: "+t2[j]);
-			}		
-			
-			//System.out.println(que);
-			//System.out.println(que.peek());
-			//System.out.println(cnt);
+
+			}					
 			cnt++;
+			que.clear();
 		}
-		
-		
-		
+					
 	}
 }
