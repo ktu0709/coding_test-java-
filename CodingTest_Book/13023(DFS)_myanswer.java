d542f8c00b67b71ffc79e418acfbbafcc183f461
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2024-05-12 18:03:53 +0900

    Create 13023(DFS)_myanswer.java

---
+++ CodingTest_Book/13023(DFS)_myanswer.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main
+{
+	static int t1,t2;
+	static boolean arrive = false;  //깊이가 5가 될 경우 true
+	static ArrayList<Integer>[] graph;
+	static boolean[] visited;
+	
+	
+	private static void dfs(int n , int depth){
+		if(depth == 5 || arrive){
+			arrive = true;
+			return;	
+		} 
+		visited[n] = true;
+	
+		for(int i : graph[n]){
+		 if(!visited[n]){
+			dfs(i,depth+1);
+			}
+		else visited[n] = false;
+		}
+	}
+	
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    
+	    t1 = Integer.parseInt(st.nextToken());
+	    t2 = Integer.parseInt(st.nextToken());
+	    
+		graph = new ArrayList[t1];
+		visited = new boolean[t1];
+		
+		for(int i=0; i< t1;i++){
+			graph[i] = new ArrayList<Integer>(); 
+		}
+		
+		for(int i=0;i<t2;i++){
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+	    	int v = Integer.parseInt(st.nextToken());
+	    	
+	    	//쌍방향
+	    	graph[u].add(v);
+	    	graph[v].add(u);
+		}
+		
+		for(int i=0;i<t1;i++){
+			dfs(i,1);
+		}
+		
+		if(arrive){
+			System.out.println(1);
+		}
+		else System.out.println(0);		        
+        
+	}
+}
