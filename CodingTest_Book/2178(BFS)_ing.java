baa9dd929125a420dd5da7b6c74ff7161894726a
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2024-05-16 11:36:51 +0900

    Create 2178(BFS)_ing.java

---
+++ CodingTest_Book/2178(BFS)_ing.java
@@ -0,0 +1,81 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	private static int t1,t2;
+	private static int[][] graph;
+	private static boolean[][] visited;
+	private static int[] dx = {0, 0, -1, 1}; // 상하좌우
+    private static int[] dy = {-1, 1, 0, 0}; // 상하좌우
+
+	private static int bfs(int x,int y){
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[]{x,y});
+		visited[x][y] = true;
+		int[][] distance = new int[t1][t2];
+		
+		
+		while(!q.isEmpty()){
+			int[] current = q.poll();
+            int currx = current[0];
+            int curry = current[1];
+            
+			for(int i=0;i<4;i++){
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				
+				if(nx >= 0 && ny >= 0 && nx < t1 && ny < t2 && !visited[nx][ny] && graph[nx][ny] == 1 ){
+					q.add(new int[]{nx,ny});
+					visited[nx][ny] = true;
+					distance[nx][ny] = distance[currx][curry]+1;
+				}
+			}
+		}
+		
+		return distance[t1-1][t2-1];
+	}
+	
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		t1 = Integer.parseInt(st.nextToken());
+		t2 = Integer.parseInt(st.nextToken());
+		int cnt = 0;
+		
+		graph = new int[t1][t2];
+		visited = new boolean[t1][t2];
+		
+		for(int i=0;i<t1;i++){
+			//st = new StringTokenizer(br.readLine());
+			String line = br.readLine();
+			for(int j=0;j<t2;j++){
+				//graph[i][j] = Integer.parseInt(st.nextToken());
+				graph[i][j] = line.charAt(j) - '0';
+			}
+		}
+		
+		
+		/*
+		//출력
+		for(int i=0;i<t1;i++){
+			for(int j=0;j<t2;j++){
+				System.out.print(graph[i][j]+ " ");
+			}
+			System.out.print("\n");
+		}
+		*/
+		
+		
+		cnt = bfs(0,0);
+		System.out.println(cnt);
+
+	}
+}
