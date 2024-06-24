4596d27482ed3cee6915a69b27fca995ad701e03
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2024-06-24 14:28:04 +0900

    Update and rename 2343(BS)_ing.java to 2343(BS).java

--- CodingTest_Book/2343(BS).java
+++ CodingTest_Book/2343(BS).java
@@ -5,30 +5,39 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Main
 {
-	static int t1 , t2 , sum;
-	static int[] arr , arr_sum;
+	static int t1 , t2 , max , sum;
+	static int[] arr;
 	
-	private static void BinarySearch(int key , int low , int high){
+	private static void BinarySearch(int low , int high){
 		int mid;
+
 		
 		while(low <= high){
-			mid = (low+high)/2;
 			
-			if(key == arr[mid]){
-				return;
+		mid = (low+high)/2;
+		int sum = 0;
+		int count = 0;
+		
+		for(int i=0;i<t1;i++){
+			if(sum + arr[i] > mid){
+				count++;
+				sum=0;
 			}
+			sum = sum + arr[i];
+		}
 			
-			else if (key < arr[mid]){
-				high = mid-1;
+			if(sum!=0) count++;
+			
+			if(count > t2){
+				low = mid +1;
 			}
-			
-			else{
-				low = mid+1;
+			else {
+				high = mid -1;
 			}
 		}
-		
+		System.out.println(low);
 	}
 	
 	public static void main (String[] args) throws java.lang.Exception
@@ -40,27 +49,14 @@ class Ideone
 		t1 = Integer.parseInt(st.nextToken());
 		t2 = Integer.parseInt(st.nextToken());
 		arr = new int[t1];
-		arr_sum = new int[t1];
-		int max = 0;
+		max = 0;
+		sum = 0;
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<t1;i++){
 			arr[i] = Integer.parseInt(st.nextToken());
 			sum += arr[i];
-			System.out.print(arr[i] + " ");
-		}
-		
-		System.out.print("\n");
-		
-		//누적 합
-		for(int i=0;i<t1;i++){
-			if(i==0){
-				arr_sum[i] = arr[i];
-			}
-			else{
-			arr_sum[i] = arr_sum[i-1] + arr[i];
-			}
-			System.out.print(arr_sum[i] + " ");
+			//System.out.print(arr[i] + " ");
 		}
 		
 		
@@ -70,8 +66,9 @@ class Ideone
 				}
 		}
 		
-		System.out.println("\n" + sum);
-		System.out.println(max);
+		BinarySearch(max,sum);
+		//System.out.println("\n" + sum);
+		//System.out.println(max);
 		
 		
 		
